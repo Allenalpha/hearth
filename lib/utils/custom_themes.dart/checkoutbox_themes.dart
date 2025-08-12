d3a04872936576e3f78709adbfa06fc10125551c
Allenalpha <allenalpha64@gmail.com>
2025-08-12 10:55:37 +0100

    feat: add checkbox theme configuration

---
+++ lib/utils/custom_themes.dart/checkoutbox_themes.dart
@@ -0,0 +1,26 @@
+import "package:flutter/material.dart";
+import "package:hearth/utils/colors.dart";
+
+
+class MyCheckboxTheme {
+  MyCheckboxTheme._();
+
+  static CheckboxThemeData checkBoxTheme = CheckboxThemeData(
+    side: const BorderSide(color: borders, width: 1.5),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return primaryColor;
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+}
