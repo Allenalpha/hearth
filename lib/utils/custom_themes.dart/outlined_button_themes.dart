44ef6c86255adc116a88bf90774259f02ed5f3fa
Allenalpha <allenalpha64@gmail.com>
2025-08-12 08:48:15 +0100

    feat: add outlined button theme configuration

---
+++ lib/utils/custom_themes.dart/outlined_button_themes.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:hearth/utils/colors.dart';
+
+
+
+class MyOutlineButtonTheme {
+  MyOutlineButtonTheme._();
+
+  static final myOutlineButtonTheme = OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      side: const BorderSide(
+        color: borders,
+        width: 1,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(24),
+      ),
+      fixedSize: const Size(150, 48),
+      textStyle: const TextStyle(
+          color: primaryColor, fontSize: 18, fontFamily: 'regular'),
+    ),
+  );
+}
