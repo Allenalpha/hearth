2704c50a8268a157a3c8e1e7c496454f71dd1b87
Allenalpha <allenalpha64@gmail.com>
2025-08-12 08:19:28 +0100

    feat: add font sizes,color and weight in text_style.dart

---
+++ lib/utils/text_style.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hearth/utils/colors.dart';
+
+
+TextStyle s20SBoldDark = GoogleFonts.inter(
+  fontWeight: FontWeight.w600,
+  color: textDark,
+  fontSize: 20,
+  height: 1.0,
+);
+
+TextStyle s16RegGrey = GoogleFonts.inter(
+  fontWeight: FontWeight.w400,
+  fontSize: 16,
+  color: appGrey,
+);
+
+TextStyle heading4 = GoogleFonts.inter(
+  fontWeight: FontWeight.w600,
+  fontSize: 28,
+  color: textDark,
+);
+
+TextStyle body2 = GoogleFonts.dmSans(
+  fontWeight: FontWeight.w400,
+  fontSize: 16,
+  color: textSecondary,
+  height: 1.5,
+);
+
+TextStyle f20w600Black = GoogleFonts.inter(
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+  color: Colors.black,
+);
+
+TextStyle inputText = GoogleFonts.inter(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  color: secondaryGrey,
+);
+
+TextStyle f20w400Primary = GoogleFonts.inter(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+  color: textPrimary,
+);
+
+TextStyle onboardingC = GoogleFonts.inter(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: onboardingColor,
+);
+
+TextStyle headingText = GoogleFonts.inter(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: textSecondary,
+);
+
+TextStyle labelText = GoogleFonts.inter(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: labelColor,
+);
+
+TextStyle ModalText = GoogleFonts.inter(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: lightblue,
+);
+
+TextStyle Modal2Text = GoogleFonts.inter(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  color: textSecondary,
+);
+
+TextStyle Modal3Text = GoogleFonts.inter(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: Colors.white ,
+);
+
+TextStyle Modal4Text = GoogleFonts.inter(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  color: Color(0xff131215) ,
+
+);
+
+TextStyle BottomText = GoogleFonts.inter(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  color: textSecondary,
+
+);
+
+TextStyle Modal5Text = GoogleFonts.inter(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  color: lightgrey ,
+);
+
+TextStyle Modal6Text = GoogleFonts.inter(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  color: Colors.black ,
+);
+
+TextStyle Modal7Text = GoogleFonts.inter(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: modalBlack ,
+);
+
+TextStyle Modal8Text = GoogleFonts.inter(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  color: textPrimary ,
+);
+
+TextStyle Modal9Text = GoogleFonts.inter(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: darkest ,
+);
+
+TextStyle Modal10Text = GoogleFonts.inter(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: textDark ,
+);
+
+TextStyle ToggleText = GoogleFonts.inter(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: toggleAsh ,
+);
+
+TextStyle searchText = GoogleFonts.inter(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  color: toggleAsh ,
+);
+
+TextStyle TermsText = GoogleFonts.inter(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  color: Color(0xff000000) ,
+);
+
+TextStyle LightText = GoogleFonts.inter(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+  color: Color(0xff000000) ,
+);
+
+
+TextStyle RulesText = GoogleFonts.inter(
+  fontSize: 11,
+  fontWeight: FontWeight.w400,
+  color: Color(0xff1E1E1E) ,
+);
+
+
+TextStyle NumberText = GoogleFonts.inter(
+  fontSize: 12,
+  fontWeight: FontWeight.w700,
+  color: Color(0xff1E1E1E) ,
+);
+
+TextStyle HeaderText = GoogleFonts.inter(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  color: Color(0xff000000) ,
+);
+
+TextStyle DashText = GoogleFonts.inter(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+  color: Color(0xff000000) ,
+);
+
+
+TextStyle DashTwoText = GoogleFonts.inter(
+  fontSize: 10,
+  fontWeight: FontWeight.w500,
+  color: Color(0xff000000) ,
+);
+
+TextStyle DashThreeText = GoogleFonts.inter(
+  fontSize: 13,
+  fontWeight: FontWeight.w500,
+  color: Color(0xff000000) ,
+);
+
+TextStyle DashFourText = GoogleFonts.inter(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  color: primaryColor ,
+);
+
+TextStyle DashFiveText = GoogleFonts.inter(
+  fontSize: 24,
+  fontWeight: FontWeight.w500,
+  color: primaryColor ,
+);
+
+
+
+
+
+
+// {
+//   FontWeight.w100: 'Thin',
+//   FontWeight.w200: 'ExtraLight',
+//   FontWeight.w300: 'Light',
+//   FontWeight.w400: 'Regular',
+//   FontWeight.w500: 'Medium',
+//   FontWeight.w600: 'SemiBold',
+//   FontWeight.w700: 'Bold',
+//   FontWeight.w800: 'ExtraBold',
+//   FontWeight.w900: 'Black',
+// }
