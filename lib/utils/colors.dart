a27f95f7123e858606dffbdc0e43cdef67dcbcae
Allenalpha <allenalpha64@gmail.com>
2025-08-14 09:25:13 +0100

    feat(color): changed the colors to match the brand color and app design

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,68 +1,46 @@
 import 'package:flutter/material.dart';
 
-const Color appWhite = Color(0xffFFFFFF);
 
-const Color textDark = Color(0xff1B0A05);
-
-const Color darkerText = Color(0xff1A2218);
 
 const Color appGrey = Color(0xff6A6A6A);
 
-const Color primaryColor = Color(0xffF1592C);
+const Color primaryColor = Color(0xff358438);
 
-const Color lighterAlt = Color(0xffFEF8F6);
+const Color lightPrimary = Color(0xffF3FAF3);
 
-const Color lighter = Color(0xffFDE4DC);
+const Color ratingColor = Color(0xffFACC15);
 
-const Color light = Color(0xffFBCCBD);
+const Color strokeColor = Color(0xffE0E0E0);
 
-const Color tertiary = Color(0xffF79A7E);
+const Color black = Color(0xff000000);
 
-const Color darkAlt = Color(0xffDA5127);
+const Color error = Color(0xffE33629);
 
-const Color dark = Color(0xffB84421);
+const Color arrowGray = Color(0xff717171);
 
-const Color darker = Color(0xff883218);
+const Color customGrey = Color(0xfff5f5f5);
 
-const Color disabled = Color(0xffA6A5A5);
+const Color inputGrey = Color(0xffFBFBFB);
 
-const Color borders = Color(0xffE0E0E0);
+const Color secondaryGrey = Color(0xfff8f8f8);
 
-const Color customGrey = Color(0xffF1F1F1);
+const Color darkGrey = Color(0xff7d7d7d);
 
-const Color arrowGrey = Color(0xff717171);
+const Color lightRed = Color(0xffFFDDDD);
 
-const Color secondaryGrey = Color(0xff7B8579);
+const Color darkRed = Color(0xffB10303);
 
-const Color textPrimary = Color(0xff1B0A05);
+const Color darkYellow = Color(0xff74580F);
 
-const Color textSecondary = Color(0xff6A6A6A);
+const Color lightYellow = Color(0xffF4FFC1);
 
-const Color onboardingColor = Color(0xffFEF8F6);
+const Color darkGreen = Color(0xff0B5C0D);
 
-const Color lightPrimary = Color(0xffFCFBFA);
+const Color lightGreen = Color(0xff0B5C0D);
 
-const Color labelColor = Color(0xff101928);
 
-const Color lightblue = Color(0xff77869E);
 
-const Color lightgrey = Color(0xff5c5c5c);
 
-const Color dragColor = Color(0xffCBCDCC80);
 
-const Color modalBlack = Color(0xff111111);
 
-const Color dividerColor = Color(0xffb2b2b2);
-
-const Color darkest = Color(0xffF15A2B);
-
-const Color buttonAsh = Color(0xffe3e0de);
-
-const Color toggleAsh = Color(0xff535353);
-
-const Color requestAsh = Color(0xffFAF6F5);
-
-const Color ratingColor = Color(0xffE0B00C);
-
-const Color backgroundAsh = Color(0xffedeae8);
 
