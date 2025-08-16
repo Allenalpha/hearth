ffceb45b2e0dca19346ad75caa5013203b2bf477
Allenalpha <allenalpha64@gmail.com>
2025-08-16 17:18:35 +0100

    feat: add IntroScreen widget with onboarding layout

---
+++ lib/auth/onboarding/intro.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hearth/auth/onboarding/onboarding.dart';
+
+import 'package:hearth/utils/colors.dart';
+
+
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({super.key});
+
+  @override
+  State<IntroScreen> createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  @override
+  void initState() {
+    super.initState();
+    onBoard();
+  }
+
+  void onBoard() async {
+    await Future.delayed(const Duration(seconds: 3), () {
+      Get.offAll(() =>  OnboardingScreen());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: primaryColor,
+        width: double.infinity,
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image(image: AssetImage("assets/images/logo.png")),
+            SizedBox(height: 20.8,),
+            Image(image: AssetImage("assets/images/hearth.png"))
+          ],
+        ),
+      ),
+    );
+  }
+}
