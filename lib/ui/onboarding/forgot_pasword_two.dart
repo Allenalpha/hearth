6ec64750b70922170f94abc149092e478b4e0788
Allenalpha <allenalpha64@gmail.com>
2025-08-25 08:14:46 +0100

    feat: add reset password screen with email input form

---
+++ lib/ui/onboarding/forgot_pasword_two.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ForgotPasswordTwoScreen extends StatefulWidget {
+  const ForgotPasswordTwoScreen({super.key});
+
+  @override
+  State<ForgotPasswordTwoScreen> createState() => _ForgotPasswordTwoScreenState();
+}
+
+class _ForgotPasswordTwoScreenState extends State<ForgotPasswordTwoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
