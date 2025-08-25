7aa2018d4ed0b03244eca60ff94feb28a50d7cba
Allenalpha <allenalpha64@gmail.com>
2025-08-25 08:18:34 +0100

    feat: add reset password screen with new password input

--- lib/ui/onboarding/forgot_pasword_two.dart
+++ lib/ui/onboarding/forgot_pasword_two.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:hearth/ui/onboarding/reset_complete.dart';
+import 'package:hearth/utils/colors.dart';
+import 'package:hearth/utils/text_style.dart';
 
 class ForgotPasswordTwoScreen extends StatefulWidget {
   const ForgotPasswordTwoScreen({super.key});
@@ -10,6 +15,55 @@ class ForgotPasswordTwoScreen extends StatefulWidget {
 class _ForgotPasswordTwoScreenState extends State<ForgotPasswordTwoScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+      final inputBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide:  BorderSide(color: Colors.grey),
+    );
+    return Scaffold(
+      backgroundColor: appWhite,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomBackButton(),
+              SizedBox(height: 20,),
+              Text("Reset password",style: HeaderText),
+              SizedBox(height: 16,),
+              Text("Please enter your email to receive instructions to reset\nyour password again.",style: Header2Text,),
+              SizedBox(height: 28,),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "New password",
+                  border: inputBorder,
+                  prefixIcon: Icon(Icons.lock_outline_rounded),
+                ),
+              ),
+              SizedBox(height: 16,),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Confirm password",
+                  border: inputBorder,
+                  prefixIcon: Icon(Icons.lock_outline_rounded),
+                ),
+              ),
+              SizedBox(height:428,),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: primaryColor
+                  ),
+                  onPressed: (){
+                    Get.to(()=>ResetCompleteScreen());
+                }, child: Text("Change password",style: ButtonText,) ),
+              )
+             ],
+           ),
+         ) ),
+     );
   }
 }
---
+++ lib/ui/onboarding/reset_complete.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResetCompleteScreen extends StatefulWidget {
+  const ResetCompleteScreen({super.key});
+
+  @override
+  State<ResetCompleteScreen> createState() => _ResetCompleteScreenState();
+}
+
+class _ResetCompleteScreenState extends State<ResetCompleteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
