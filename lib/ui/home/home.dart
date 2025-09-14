2aae78fe3bf44a244b9da56ae59b1bfb15d05f87
Allenalpha <allenalpha64@gmail.com>
2025-09-14 20:54:43 +0100

    feat: add ApartmentScreen with navigation

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:hearth/ui/category/Lease.dart';
+import 'package:hearth/ui/category/apartment.dart';
 import 'package:hearth/ui/category/land.dart';
 import 'package:hearth/ui/category/rental.dart';
 import 'package:hearth/utils/colors.dart' as AppColors;
@@ -125,7 +126,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 GestureDetector(
                   onTap: (){
-                    Get.to(()=>());
+                    Get.to(()=>ApartmentScreen());
                   },
                   child: CategoryButton(icon: Icons.business, label: "Commercial"),
                 ),
