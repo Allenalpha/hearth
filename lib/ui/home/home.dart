c4574171d77767019c30c354d525585ea7212103
Allenalpha <allenalpha64@gmail.com>
2025-09-14 20:40:06 +0100

    feat: add landScreen with navigation

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:hearth/ui/category/Lease.dart';
+import 'package:hearth/ui/category/land.dart';
 import 'package:hearth/ui/category/rental.dart';
 import 'package:hearth/utils/colors.dart' as AppColors;
 import 'package:hearth/utils/colors.dart';
@@ -118,7 +119,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 GestureDetector(
                   onTap: (){
-                    Get.to(()=>());
+                    Get.to(()=>LandScreen());
                   },
                   child: CategoryButton(icon: Icons.landscape, label: "Land"),
                 ),
